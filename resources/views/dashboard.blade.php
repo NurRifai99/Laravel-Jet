70b422eb4aa53fbc904c73f15dc92bbc90ded6dd
fai <nurrahmanrifai3@gmail.com>
2025-02-22 23:25:41 +0700

    create livewire

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="w-full h-16 my-5 shadow-lg bg-gray-200 flex justify-center items-center font-bold">
+        Dashboard dekdek
+    </div>
+</x-app-layout>
